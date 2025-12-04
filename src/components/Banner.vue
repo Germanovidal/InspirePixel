f968154a47c26495fd5c7e23baa944816721513a
Germano <ggermano.vidal@gmail.com>
2025-12-04 00:58:57 -0300

    estiliza banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -6,7 +6,7 @@ import fotografo from "../assets/banner-img.jpg"
     <section class="banner">
         <div>
             <h1>Onde a paisagem vira
-                <span>inspiração</span> </h1>
+                <span>inspiração</span>. </h1>
                 <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
         </div>
         <img :src="fotografo" alt="">
@@ -20,7 +20,24 @@ import fotografo from "../assets/banner-img.jpg"
         margin: 2rem;
     }
 
+    div { 
+        width: 45%;
+    }
+
     h1{
         font-size: 3rem;
+        margin-top: 4rem;
+        margin-bottom: 1rem;
+
+        span {
+            color: #E1306C;
+            font-size: 4.3rem;
+        }
+    }
+
+    img{
+        margin-top: 2.5rem;
+        width: 45%;
+        border-radius: 86px;
     }
 </style>
